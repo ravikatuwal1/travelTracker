116be169a2128408075b3cc72b3ef9d77b3f4505
ravikatuwal1 <ravikatuwal9@gmail.com>
2025-05-25 17:54:50 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from 'pg';
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "PostGres@123",
+  port: 5432,
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let countriesVisited = [];
+let total;
+
+async function checkVisited(){
+  const result = (await db.query("SELECT country_code FROM visited_countries")).rows;
+  
+  let countries = [];
+  result.forEach( country => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisited();
+  res.render("index.ejs", {
+    total: countries.length,
+    countries: countries,
+  });
+});
+
+//INSERT new Country
+app.post("/add", async (req, res) => {
+  let newVisit = req.body["country"];
+try{
+  const result = await db.query("SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';", [newVisit.toLowerCaer()]);
+  const data = result.rows[0];
+  const countryCode = data.country_code;
+  try {
+  await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [countryCode]);
+  res.redirect("/");
+}catch (err){
+  console.log(err);
+  const countries = await checkVisited();
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    error: "Country has already been added, Try Again."
+  });
+}
+}catch(err){
+  console.log(err);
+  const countries = await checkVisited();
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    error: "Country name does not exist, try again.",
+  });
+}
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
